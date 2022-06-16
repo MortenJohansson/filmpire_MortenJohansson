862d749f69f73ead0085af428ebc89764b9c0575
Morten Johansson <morten.johansson@outlook.com>
2022-06-16 11:29:29 +0200

    Redux setup

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
+import { useSelector } from 'react-redux';
+
+import { useGetMoviesQuery } from '../../services/TMDB';
 
 function Movies() {
+  const { data } = useGetMoviesQuery();
+
+  // eslint-disable-next-line no-console
+  console.log(data);
   return <div>Movies</div>;
 }
 
